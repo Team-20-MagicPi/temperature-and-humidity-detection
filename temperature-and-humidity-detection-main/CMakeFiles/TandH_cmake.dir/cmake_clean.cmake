358f4d215088ac4b2f9ecb489a106877c65bf160
Touru97 <47893261+Touru97@users.noreply.github.com>
2021-04-19 22:53:59 +0100

    Add files via upload

---
+++ temperature-and-humidity-detection-main/CMakeFiles/TandH_cmake.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TandH_cmake.dir/temperature_and_humidity_detection.cpp.o"
+  "TandH_cmake.pdb"
+  "TandH_cmake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TandH_cmake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
